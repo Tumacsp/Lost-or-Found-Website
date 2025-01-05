3cf5b54aa60b31e8f7b35b773859f5404855caf7
japan0101 <japan.pon11@gmail.com>
2025-01-05 11:35:22 +0700

    model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Location(models.Model):
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longtitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    bodyText = models.TextField()
+    picture_name = models.CharField(max_length=100)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.OneToOneField(Location, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    status  = models.CharField(max_length=100)
+    created_at = models.CharField(max_length=100)
