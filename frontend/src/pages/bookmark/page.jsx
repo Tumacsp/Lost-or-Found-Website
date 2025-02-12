52416da7266d2eb144faeb3143eba739198b90ae
japan0101 <japan.pon11@gmail.com>
2025-02-12 13:32:43 +0700

    Bookmark Page

--- frontend/src/pages/bookmark/page.jsx
+++ frontend/src/pages/bookmark/page.jsx
@@ -1,21 +1,50 @@
-import {React, useState} from 'react';
+import {React, useState, useEffect} from "react";
 import Navbar from '../../components/navbar';
 import Footer from '../../components/footer';
-import Poster from '../card';
-
+import axiosInstance from "../../utils/axios";
 import { handleError } from "../../utils/errorHandler";
+import { useNavigate } from "react-router-dom";
+import { makeCard } from "../card";
+
 const Bookmark = () => {
+    const [error, setError] = useState("");
+    const [terms, setTerm] = useState('');
+    const navigate = useNavigate();
+    const [postsData, setPostData] = useState([])
+    function search(){
+        console.log("Finding Post with Title:" + terms)
+        fetchPost()
+    }
+    const fetchPost = async() =>{
+      try {
+          const response = await axiosInstance.get("api/bookmark/");
+          setPostData(response.data)
+          console.log("Post Found", response.data)
+          setError("");
+      } catch (err) {
+          handleError(err, setError, navigate);
+      }
+    }
+    useEffect(() => {
+        fetchPost();
+      }, []);
+
+    const cards = postsData.map((data) => makeCard(data.id, data.picture_name, data.category, data.title, data.reward));
+    function Result(){
+      if(postsData.length > 0){
+        return cards
+      }else{
+        return (<h3 className="text-lg sm:text-xl font-semibold mb-2">No Posts Bookmarked</h3>)
+      }
+    }
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="flex-grow">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
-          <h2 className="text-2xl sm:text-3xl lg:text-4xl font-bold text-center mb-4">Bookmark</h2>
-          <p className="text-center text-gray-700 mb-6 max-w-2xl mx-auto"> 
-            Easily report lost items or find something you've misplaced. Start by exploring the options below.
-          </p>
-          <div>
-            <Poster />
+          <h2 className="text-2xl sm:text-3xl lg:text-4xl font-bold text-center mb-4">Bookmarked Posts</h2>
+          <div className='flex flex-wrap'>
+            <Result />
           </div>
         </div>
       </main>
