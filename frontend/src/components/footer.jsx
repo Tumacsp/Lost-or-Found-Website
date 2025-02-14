0531c2a2d2a9ed015e53029b70470143c07a64ea
japan0101 <japan.pon11@gmail.com>
2025-02-14 21:14:58 +0700

    add credit

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -4,7 +4,12 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 p-4 mt-8">
       <div className="container mx-auto text-center text-white">
-        <p>&copy; {new Date().getFullYear()} Lost or Found. All rights reserved.</p>
+        <p>&copy; 2025 Lost or Found. All rights reserved.</p>
+      </div>
+      <h1 className='text-white text-center my-2'>Developers</h1>
+      <div className='grid md:grid-cols-2 gap-6 max-w-6xl mx-auto'>
+        <p className='text-white text-center'>65070064 <br/>ณภัทร เวชพันธุ์</p>
+        <p className='text-white text-center'>65070118 <br/>นันทพงศ์ วิเศษมงคลชัย</p>
       </div>
     </footer>
   );
